fa26b72dde33df1bc3c41c9a1ffede1168984528
RAIMOVKHASAN <hrk.itnnes@gmail.ru>
2023-05-25 20:54:11 +0300

    Initial Commit

---
+++ fanfinctionapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fanfinctionapp
+//
+//  Created by mac on 25.05.2023.
+//  Copyright © 2023 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
